a999c059478d2fd5675d1bc337576cd300b17167
spiritlhl <103393591+spiritLHLS@users.noreply.github.com>
2025-08-10 14:11:13 +0000

    feat: 尝试直接捕捉输出内容，方便后续并发执行请求

--- cmd/main.go
+++ cmd/main.go
@@ -49,9 +49,10 @@ func main() {
 	}
 	result := nt.TraceRoute(language, location, checkType)
 	for _, res := range result {
-		if strings.Contains(res, "ICMP") || (strings.Contains(res, "NextTrace") && strings.Contains(res, "API")) {
-			fmt.Print(res)
+		if strings.TrimSpace(res) != "" && strings.Contains(res, "ICMP") { // || (strings.Contains(res, "NextTrace") && strings.Contains(res, "API"))
+			fmt.Print(strings.ReplaceAll(res, "\n", ""))
+		} else if strings.TrimSpace(res) != "" {
+			fmt.Println(res)
 		}
-		fmt.Println(res)
 	}
 }
